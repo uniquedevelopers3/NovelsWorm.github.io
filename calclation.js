6003716945776481bdb04c2cb5689abbf809b246
Z00Z1 <149786518+Z00Z1@users.noreply.github.com>
2023-11-30 21:26:34 +0400

    Add files via upload

---
+++ calclation.js
@@ -0,0 +1,6 @@
+var count = 0;
+            function claculatePrice(){
+                count +=1;
+            
+                document.getElementById("count").innerHTML=count * 0.500;
+            }
