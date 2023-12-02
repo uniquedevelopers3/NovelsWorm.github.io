e7e381fe9446ed6bdf152c0ea52d8e35826f5810
MaryamHamed4 <150849375+MaryamHamed4@users.noreply.github.com>
2023-12-02 16:32:52 +0400

    Add files via upload

--- table.js
+++ table.js
@@ -5,20 +5,23 @@ function novelInfo (title, author, genre){
 }
 
 let novelArray = [
-    new novelInfo("Phantom", "Jo Nesbo", "Crime fiction")
+    new novelInfo("Phantom", "Jo Nesbo", "Crime fiction"),
+    new novelInfo("Daddy long legs", "Jean Webster", "Young adult"),
 ];
 
 // function to disply Info of the array
-function bookDisply(){
+function bookDisplay(){
     let tableBody = document.getElementById("table").getElementsByTagName('tbody')[0];
-    // clear rows
-    tableBody.innerHTML = "";
 
-    novelArray.forEach(item =>{
-        let row = tableBody.insertRow();
-        row.insertCell(0).textContent = item.title;
-        row.insertCell(1).textContent = item.author;
-        row.insertCell(2).textContent = item.genre;
+    novelArray.forEach(function (novelInfo){
+        var row = tableBody.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+
+        cell1.innerHTML = novelInfo.title;
+        cell2.innerHTML = novelInfo.author;
+        cell3.innerHTML = novelInfo.genre;
     });
 }
 
@@ -34,21 +37,44 @@ function addData(){
     // add the new item to the array
     novelArray.push(newNovel);
 
-    bookDisply();
+    // Clear input fields
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("genre").value = "";
+
+    //refresh the table
+    refreshBookDisplay();
+}
+
+function refreshBookDisplay() {
+    var tableBody = document.getElementById("table").getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = ""; // clear existing table rows
+    bookDisplay(); // disply the updated data
 }
 
 // function to search info
 function searchData(){
-    let searchQuery = document.getElementById("searchQuery").value;
+    let searchTitle = document.getElementById("searchTitle").value;
     // filter the array
-    let searchResult = novelArray.filter(item =>
-            item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            item.author.toLowerCase().includes(searchQuery.toLowerCase()) ||
-            item.genre.toLowerCase().includes(searchQuery.toLowerCase()) 
-        );
+    let searchResult = novelArray.filter(function (novelInfo){
+            return novelInfo.title.toLowerCase().includes(searchTitle.toLowerCase());
+        });
     // disply results:
-    novelArray = searchResult; 
-    bookDisply();
+    displayResults(searchResult, "table");
+}
+
+// function to display search results
+function displayResults(results, tableName){
+    var tableBody = document.getElementById("table").getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = "";
+
+    results.forEach(function (result){
+        var row = tableBody.insertRow();
+        for(var key in result){
+            var cell = row.insertCell();
+            cell.innerHTML = result[key];
+        }
+    });
 }
 
 
